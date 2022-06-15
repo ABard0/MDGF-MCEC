59426a27203c24536cb9124d91e9046f46c73016
ABard <39459832+ABard0@users.noreply.github.com>
2022-06-15 16:36:25 +0800

    Update prepareData2.py

--- code/circRNA2Disease/prepareData2.py
+++ code/circRNA2Disease/prepareData2.py
@@ -33,6 +33,8 @@ def get_edge_index(matrix):
     return t.LongTensor(edge_index)
 
 
+
+
 def prepare_data(opt):
     dataset = dict()
     dataset['md_p'] = read_csv('../../datasets/circRNA2Disease/CircRNA2Disease_Association.csv')
@@ -52,153 +54,49 @@ def prepare_data(opt):
     one_tensor = t.LongTensor(one_index)
     dataset['md'] = dict()
     dataset['md']['train'] = [one_tensor, zero_tensor]
-    c1 = []
-    c2 = []
-    c3 = []
-    mm_matrix = read_txt('../../datasets/circRNA2Disease/multiview/CircRNA2Disease_circFSsimilarity.txt')
-    y_pred = KMeans(n_clusters=3, random_state=9).fit_predict(mm_matrix)
-    y_pred = list(y_pred)
 
-    for i in range(len(y_pred)):
-        if y_pred[i] == 0:
-            c1.append(i)
-        elif y_pred[i] == 1:
-            c2.append(i)
-        else:
-            c3.append(i)
-    for i in range(len(c1)):
-        row = c1[i]
-        for j in range(len(c1)):
-            col = c1[j]
-            if row != col:
-                mm_matrix[row][col] = 1.8*mm_matrix[row][col]
-    for i in range(len(c2)):
-        row = c2[i]
-        for j in range(len(c2)):
-            col = c2[j]
-            if row != col:
-                mm_matrix[row][col] = 1.8*mm_matrix[row][col]
-    for i in range(len(c3)):
-        row = c3[i]
-        for j in range(len(c3)):
-            col = c3[j]
-            if mm_matrix[row][col] != 1:
-                mm_matrix[row][col] = 1.8*mm_matrix[row][col]
-    mm_matrix = t.FloatTensor(mm_matrix)
-    mm_edge_index = get_edge_index(mm_matrix)
-    dataset['mm_f'] = {'data_matrix': mm_matrix, 'edges': mm_edge_index}
+    def createSimilarityInfo(Dataset=dataset, path='', name=''):
+        c1 = []
+        c2 = []
+        c3 = []
+        mm_matrix = read_txt(path)
+        y_pred = KMeans(n_clusters=3, random_state=9).fit_predict(mm_matrix)
+        y_pred = list(y_pred)
 
+        for i in range(len(y_pred)):
+            if y_pred[i] == 0:
+                c1.append(i)
+            elif y_pred[i] == 1:
+                c2.append(i)
+            else:
+                c3.append(i)
+        for i in range(len(c1)):
+            row = c1[i]
+            for j in range(len(c1)):
+                col = c1[j]
+                if row != col:
+                    mm_matrix[row][col] = 1.8*mm_matrix[row][col]
+        for i in range(len(c2)):
+            row = c2[i]
+            for j in range(len(c2)):
+                col = c2[j]
+                if row != col:
+                    mm_matrix[row][col] = 1.8*mm_matrix[row][col]
+        for i in range(len(c3)):
+            row = c3[i]
+            for j in range(len(c3)):
+                col = c3[j]
+                if mm_matrix[row][col] != 1:
+                    mm_matrix[row][col] = 1.8*mm_matrix[row][col]
+        mm_matrix = t.FloatTensor(mm_matrix)
+        mm_edge_index = get_edge_index(mm_matrix)
+        Dataset[name] = {'data_matrix': mm_matrix, 'edges': mm_edge_index}
 
+    createSimilarityInfo(dataset, '../../datasets/circ2Disease/multiview/CircRNA2Dasease_circFSsimilarity.txt', 'mm_f')
+    createSimilarityInfo(dataset, '../../datasets/circ2Disease/multiview/CircRNA2Dasease_circGIPsimilarity.txt', 'mm_g')
+    createSimilarityInfo(dataset, '../../datasets/circ2Disease/multiview/CircRNA2Dasease_disSSsimilarity.txt', 'dd_f')
+    createSimilarityInfo(dataset, '../../datasets/circ2Disease/multiview/CircRNA2Dasease_disGIPsimilarity.txt', 'dd_g')
 
-    c1 = []
-    c2 = []
-    c3 = []
-    mm_matrix = read_txt('../../datasets/circRNA2Disease/multiview/CircRNA2Disease_circGIPsimilarity.txt')
-    y_pred = KMeans(n_clusters=3, random_state=9).fit_predict(mm_matrix)
-    y_pred = list(y_pred)
-    for i in range(len(y_pred)):
-        if y_pred[i] == 0:
-            c1.append(i)
-        elif y_pred[i] == 1:
-            c2.append(i)
-        else:
-            c3.append(i)
-    for i in range(len(c1)):
-        row = c1[i]
-        for j in range(len(c1)):
-            col = c1[j]
-            if row != col:
-                mm_matrix[row][col] = 1.8 * mm_matrix[row][col]
-    for i in range(len(c2)):
-        row = c2[i]
-        for j in range(len(c2)):
-            col = c2[j]
-            if row != col:
-                mm_matrix[row][col] = 1.8 * mm_matrix[row][col]
-    for i in range(len(c3)):
-        row = c3[i]
-        for j in range(len(c3)):
-            col = c3[j]
-            if mm_matrix[row][col] != 1:
-                mm_matrix[row][col] = 1.8 * mm_matrix[row][col]
-    mm_matrix = t.FloatTensor(mm_matrix)
-    mm_edge_index = get_edge_index(mm_matrix)
-    dataset['mm_g'] = {'data_matrix': mm_matrix, 'edges': mm_edge_index}
-
-
-    c1 = []
-    c2 = []
-    c3 = []
-    # dd_matrix = read_txt(opt.data_path + '/multiview/disease semantic similarity.txt')
-
-    dd_matrix = read_txt('../../datasets/circRNA2Disease/multiview/CircRNA2Disease_disSSsimilarity.txt')
-    y_pred = KMeans(n_clusters=3, random_state=9).fit_predict(dd_matrix)
-    y_pred = list(y_pred)
-
-    for i in range(len(y_pred)):
-        if y_pred[i] == 0:
-            c1.append(i)
-        elif y_pred[i] == 1:
-            c2.append(i)
-        else:
-            c3.append(i)
-    for i in range(len(c1)):
-        row = c1[i]
-        for j in range(len(c1)):
-            col = c1[j]
-            if row != col:
-                dd_matrix[row][col] = 1.8 * dd_matrix[row][col]
-    for i in range(len(c2)):
-        row = c2[i]
-        for j in range(len(c2)):
-            col = c2[j]
-            if row != col:
-                dd_matrix[row][col] = 1.8 * dd_matrix[row][col]
-    for i in range(len(c3)):
-        row = c3[i]
-        for j in range(len(c3)):
-            col = c3[j]
-            if dd_matrix[row][col] != 1:
-                dd_matrix[row][col] = 1.8 * dd_matrix[row][col]
-    dd_matrix = t.FloatTensor(dd_matrix)
-    dd_edge_index = get_edge_index(dd_matrix)
-    dataset['dd_f'] = {'data_matrix': dd_matrix, 'edges': dd_edge_index}
-
-
-    c1 = []
-    c2 = []
-    c3 = []
-    dd_matrix = read_txt('../../datasets/circRNA2Disease/multiview/CircRNA2Disease_disGIPsimilarity.txt')
-    y_pred = KMeans(n_clusters=3, random_state=9).fit_predict(dd_matrix)
-    y_pred = list(y_pred)
-    for i in range(len(y_pred)):
-        if y_pred[i] == 0:
-            c1.append(i)
-        elif y_pred[i] == 1:
-            c2.append(i)
-        else:
-            c3.append(i)
-    for i in range(len(c1)):
-        row = c1[i]
-        for j in range(len(c1)):
-            col = c1[j]
-            if row != col:
-                dd_matrix[row][col] = 1.8 * dd_matrix[row][col]
-    for i in range(len(c2)):
-        row = c2[i]
-        for j in range(len(c2)):
-            col = c2[j]
-            if row != col:
-                dd_matrix[row][col] = 1.8 * dd_matrix[row][col]
-    for i in range(len(c3)):
-        row = c3[i]
-        for j in range(len(c3)):
-            col = c3[j]
-            if dd_matrix[row][col] != 1:
-                dd_matrix[row][col] = 1.8 * dd_matrix[row][col]
-
-    dd_matrix = t.FloatTensor(dd_matrix)
-    dd_edge_index = get_edge_index(dd_matrix)
-    dataset['dd_g'] = {'data_matrix': dd_matrix, 'edges': dd_edge_index}
     return dataset
+
 
